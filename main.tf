3f3cc9aac35c849989d0753d95ac4658f430e372
James Buchanan <jbuchanan@newrelic.com>
2022-04-08 13:05:45 +0100

    init

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = "~> 1.1.7"
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 2.35.0"
+    }
+    graphql = {
+      source = "sullivtr/graphql"
+      version = "2.4.0" #This must be 2.4.0, 2.5.0+ does not work. See issue https://github.com/sullivtr/terraform-provider-graphql/issues/70
+    }
+  }
+}
+
+provider "newrelic" {
+  region = "US" 
+}
+variable "accountId" { type = string }
+variable "NEW_RELIC_API_KEY" { type = string }
+
+provider "graphql" {
+  url = "https://api.newrelic.com/graphql"
+  headers = {
+    "API-Key" = var.NEW_RELIC_API_KEY
+  }
+}
+
+resource "graphql_mutation" "dashboard" {
+  mutation_variables = {
+    dashboard = templatefile("${path.module}/dashQueries/dash.json",{
+      DASHBOARD_NAME = "GraphQL Provider Dash" 
+      widgets = [
+        {
+          name = "Amazon traffic"
+          accountId = 2460987
+          whereClause ="where api like 'amazon%'"
+        },
+        {
+          name = "Google traffic"
+          accountId = 2460987
+          whereClause ="where api like 'google%'"
+        }
+      ]
+    }) 
+  }
+  compute_mutation_keys = {
+    "guid" = "dashboardCreate.entityResult.guid"  # The guid from the the create is used for update, read, destroy
+  }
+  compute_from_create = true
+  create_mutation = file("./dashQueries/createMutation.gql")
+  update_mutation = file("./dashQueries/updateMutation.gql")
+  delete_mutation = file("./dashQueries/deleteMutation.gql")
+  read_query      = file("./dashQueries/readQuery.gql")
+}
+
+output "dashboard_guid" {
+  value=graphql_mutation.dashboard.computed_read_operation_variables.guid
+}
+
+ 
---
+++ readme.md
@@ -0,0 +1,27 @@
+# New Relic Terraform JSON Dashboards
+This example project demontrates how you can use a [Terraform GraphQL provider](https://registry.terraform.io/providers/sullivtr/graphql/latest/docs) to provision New Relic Dashboards from JSON templates.
+
+Note: This only works with version 2.4.0 of the GraphQL provider. Version 2.5.0 adds  feature that is incompatible. See [Github issue](https://github.com/sullivtr/terraform-provider-graphql/issues/70).
+
+## Installation
+Make sure terraform is installed. I recommend [tfenv](https://github.com/tfutils/tfenv) for managing your terraform binaries.
+
+Run terraform how you usually do, or use the helper script to run terraform with the correct vars:  Update the `runtf.sh.sample` file with your credentials and accont details and rename it `runtf.sh`. **Important do not commit this new file to git!** (It should be ignored in `.gitignore` already)
+
+
+## Initialisation
+Use the `runtf.sh` helper script where ever you would normally run `terraform`. It simply wraps the terraform with some environment variables that make it easier to switch between projects.
+
+First initialise terraform:
+```
+./runtf.sh init
+```
+
+Now apply the changes:
+```
+./runtf.sh apply
+```
+
+## State storage
+This demo does not include remote state storage. State will be stored locally.
+
